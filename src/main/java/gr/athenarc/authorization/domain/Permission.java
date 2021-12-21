0e504e0ddbf6e2dffd83d9f724dfc91bd3fc0319
Konstantinos Spyrou <spyroukon@gmail.com>
2021-12-21 23:57:05 +0200

    Revert "Revert "1. refactoring  2. added subject group to permission""
    This reverts commit 77c8a10906bba87ba908dce9c214280b521f7b83.

--- src/main/java/gr/athenarc/authorization/domain/Permission.java
+++ src/main/java/gr/athenarc/authorization/domain/Permission.java
@@ -3,21 +3,31 @@ package gr.athenarc.authorization.domain;
 import javax.persistence.*;
 
 @Entity
-public class AuthTriple {
+public class Permission {
+    private static final String DEFAULT_GROUP = "default";
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String subjectGroup = DEFAULT_GROUP;
 
     private String subject;
     private String action;
     private String object;
 
-    public AuthTriple() {}
+    public Permission() {}
 
-    public AuthTriple(String subject, String action, String object) {
+    public Permission(String subject, String action, String object) {
         this.subject = subject;
         this.action = action;
         this.object = object;
+    }
+
+    public Permission(String subject, String action, String object, String subjectGroup) {
+        this.subject = subject;
+        this.action = action;
+        this.object = object;
+        this.subjectGroup = subjectGroup;
     }
 
     public Long getId() {
@@ -26,6 +36,14 @@ public class AuthTriple {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getSubjectGroup() {
+        return subjectGroup;
+    }
+
+    public void setSubjectGroup(String group) {
+        this.subjectGroup = group;
     }
 
     public String getSubject() {
